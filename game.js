e0e1c69c7e39cd3150e71f8859595e52009c6b4d
LopasReng <galo24bj@student.ju.se>
2024-11-27 11:02:56 +0100

    Core content to get started

--- game.js
+++ game.js
@@ -1,7 +1,27 @@
+//game logig variables
+
+
 function setup() {
   createCanvas(800, 600);
 }
 
+//Ambulance
+function ambulance(){
+  fill (250, 250, 0);
+  noStroke();
+  rect (100, 100, 100, 200);
+  //window
+  fill(0);
+  noStroke();
+  rect (110, 110, 80, 50);
+
+}
+
+function map(){
+
+}
+
 function draw() {
   background(255, 140, 0);
+  ambulance();
 }
