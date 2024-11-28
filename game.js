9bcf90d0bb939b91cddfd2e0d515c954ee867aff
BenjiDaHacker <gube24bx@student.ju.se>
2024-11-28 12:44:10 +0100

    Added movement to temporary map

--- game.js
+++ game.js
@@ -1,13 +1,14 @@
 // game logic variables
 
-let y = 350;
-let x = 350;
+let y = 0;
+let x = 0;
 let angle = 0;
 
 function setup() {
   createCanvas(1000, 1000);
-  background(255,255,255);
-  angleMode(DEGREES); // set the angle mode to degrees.
+  angleMode(DEGREES);
+  rectMode(CENTER); // set the angle mode to degrees.
+  frameRate(60);
 }
 
 // switch cases that moves the map when pressing WASD.
@@ -17,41 +18,49 @@ function movemap(){
 switch(key){
 
 case "w":
-y += -1;
-
-break;
-
-case "s":
 y += +1;
 
 break;
 
+case "s":
+y += -1;
+
+break;
+
 case "a":
-  rotate(angle += 0.1);
-
-  break;
-
-case "d":
   rotate(angle += -0.1);
 
-}
+break;
+
+case "d":
+  rotate(angle += +0.1);
 
 }
 
-// temporary function that draws the map
+}
+
+// function that draws the placeholder map
 
 function map(){
 push();
+translate(width /2, height /2);
+rotate(angle);
 fill (150, 150, 150);
 noStroke();
 rect (x, y, 500, 500);
 pop();
 }
 
+// function that draws the placeholder ambulance
+
+function tempAmb(){
+fill (0);
+rect(350, 350, 10, 20);
+}
 
 
 function draw() {
   background(255,255,255);
-  movemap();
   map();
+  movemap();
 }
