20cba568c371ad744fbf6f00041a0f4f26ae7b7d
BenjiDaHacker <gube24bx@student.ju.se>
2024-11-29 21:12:55 +0100

    Added gridsystem

--- game.js
+++ game.js
@@ -2,10 +2,27 @@
 
 let y = 0;
 let x = 0;
+let a = x;
+let b = y;
 let angle = 0; //Map rotation angle
 let ambulance;
 let gameMap;
-let speed = 5;
+let Road_Jump;
+let speed = 3;
+let tileArray = [
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+]; // A nested array that holds the tiles
+
+function preload() {
+  tile = loadImage("Road_Jump.png");
+}
 
 function setup() {
   createCanvas(1000, 1000);
@@ -14,6 +31,8 @@ function setup() {
   frameRate(60);
   ambulance = loadImage("Ambulance.png");
   gameMap = loadImage("TestTrack.png");
+
+  grid();
 }
 
 // switch cases that moves the map when pressing WASD.
@@ -34,12 +53,10 @@ function movemap() {
 
     case "a":
       angle += 1; //rotate the map left
-
       break;
 
     case "d":
       angle -= 1; //rotate the map right
-
       break;
   }
 }
@@ -47,26 +64,54 @@ function movemap() {
 // function that draws the map
 function map() {
   push();
+  scale(0.1);
   rotate(angle);
   image(gameMap, x, y);
+  // translate(gameMap, 1000, 1000);
   pop();
 }
 
 // function that draws the ambulance
 class Amb {
-  constractor() {}
+  constructor() {}
   draw() {
     push();
     scale(0.5);
-    image(ambulance, 1000, 1000);
+    image(ambulance, a, b);
     pop();
+  }
+}
+
+class Tile {
+  constructor(posX, posY, size, surface) {
+    this.surface = surface;
+    this.size = size;
+    this.posX = posX;
+    this.posY = posY;
+  }
+  draw() {
+    image(tile, this.posX, this.posY, this.size, this.size);
+  }
+}
+
+// Grid system
+function grid() {
+  for (let i = 0; i < 8; i++) {
+    for (let o = 0; o < 8; o++) {
+      tileArray[i][o] = new Tile(i * 50, o *50, 50, "Road_Jump.png");
+    }
   }
 }
 
 let amb = new Amb();
 
 function draw() {
-  map();
+  //map();
   movemap();
-  amb.draw();
+  //amb.draw();
+  for (let i = 0; i < 8; i++) {
+    for (let o = 0; o < 8; o++) {
+      tileArray[i][o].draw();
+    }
+  }
 }
